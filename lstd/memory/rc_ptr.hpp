5a8a70ede4b9fdf9ced577a5830b1907572599b1
Luis Diaz <41093870+LDiazN@users.noreply.github.com>
2025-11-09 20:29:33 +0100

    Add the right namespace to rc counter

--- lstd/memory/rc_ptr.hpp
+++ lstd/memory/rc_ptr.hpp
@@ -1,15 +1,18 @@
 #ifndef SHARED_PTR_HPP
 #define SHARED_PTR_HPP
 
-template <typename T>
-class RcPtr {
+namespace lstd {
+    template <typename T>
+    class RcPtr
+    {
+    };
 
-};
-
-template <typename T>
-struct RcPtrEntry {
-    size_t count = 0;
-    T* ptr = nullptr;
-};
+    template <typename T>
+    struct RcPtrEntry
+    {
+        size_t count = 0;
+        T *ptr = nullptr;
+    };
+}
 
 #endif
